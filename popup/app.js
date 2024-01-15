49987ca6ebb9307d82e6b21c98b4b00f22fb0d59
ASHUTOSH KATOCH <147313006+Xphenomenal008@users.noreply.github.com>
2024-01-15 22:17:17 +0530

    Add files via upload

---
+++ popup/app.js
@@ -0,0 +1,9 @@
+let box=document.querySelector(".box")
+let btn=document.getElementById("btn")
+function clickit(){
+box.classList.add("boxy")
+
+}
+function hide(){
+    box.classList.remove("boxy")
+}
